973b868ed8b62141c7244fd02c08f7cb2f941fa1
MarvAmBass <marvambass@gmail.com>
2014-12-01 14:05:40 +0100

    tidy up start script

--- startup-owncloud.sh
+++ startup-owncloud.sh
@@ -99,35 +99,31 @@ fi
 ###
 # Headless initialization
 ###
-if [ $(mysql -h $OWNCLOUD_MYSQL_HOST -P $OWNCLOUD_MYSQL_PORT -u $OWNCLOUD_MYSQL_USER -p$OWNCLOUD_MYSQL_PASSWORD $OWNCLOUD_MYSQL_DBNAME -e "show tables;" 2> /dev/null | wc -l) -lt 10 ]
-then
-  sleep 1
-  nginx > /dev/null 2> /dev/null &
-  sleep 1
-	## Create OwnCloud Installation
-	echo ">> init owncloud installation"
-	DATA_DIR=/usr/share/nginx/html$OWNCLOUD_RELATIVE_URL_ROOT\data
+echo ">> initialization"
+echo ">> starting nginx to configure owncloud"
+sleep 1
+nginx > /dev/null 2> /dev/null &
+sleep 1
 
-  if [ -z ${OWNCLOUD_MYSQL_USER+x} ] || [ -z ${OWNCLOUD_MYSQL_PASSWORD+x} ]
-  then
-    echo ">> using sqlite DB"
-  	DB_TYPE="sqlite"
-  	POST=`echo "install=true&adminlogin=$OWNCLOUD_ADMIN&adminpass=$OWNCLOUD_ADMIN_PASSWORD&adminpass-clone=$OWNCLOUD_ADMIN_PASSWORD&directory=$DATA_DIR&dbtype=$DB_TYPE&dbuser=&dbpass=&dbpass-clone=&dbname=&dbhost=localhost"`
-  else
-    echo ">> using mysql DB"
-  	DB_TYPE="mysql"
-  	POST=`echo "install=true&adminlogin=$OWNCLOUD_ADMIN&adminpass=$OWNCLOUD_ADMIN_PASSWORD&adminpass-clone=$OWNCLOUD_ADMIN_PASSWORD&directory=$DATA_DIR&dbtype=$DB_TYPE&dbuser=$OWNCLOUD_MYSQL_USER&dbpass=$OWNCLOUD_MYSQL_PASSWORD&dbpass-clone=$OWNCLOUD_MYSQL_PASSWORD&dbname=$OWNCLOUD_MYSQL_DBNAME&dbhost=$OWNCLOUD_MYSQL_HOST:$OWNCLOUD_MYSQL_PORT"`
-  fi
-  
-	echo "# wget -O - --no-check-certificate --no-proxy --post-data \"$POST\" "https://localhost$OWNCLOUD_RELATIVE_URL_ROOT\index.php
-  wget -O - --no-check-certificate --no-proxy --post-data "$POST" https://localhost$OWNCLOUD_RELATIVE_URL_ROOT\index.php > /dev/null 2> /dev/null
-  
-	sleep 1
-	killall nginx
+## Create OwnCloud Installation
+echo ">> init owncloud installation"
+DATA_DIR=/usr/share/nginx/html$OWNCLOUD_RELATIVE_URL_ROOT\data
+
+if [ -z ${OWNCLOUD_MYSQL_USER+x} ] || [ -z ${OWNCLOUD_MYSQL_PASSWORD+x} ]
+then
+  echo ">> using sqlite DB"
+	DB_TYPE="sqlite"
+	POST=`echo "install=true&adminlogin=$OWNCLOUD_ADMIN&adminpass=$OWNCLOUD_ADMIN_PASSWORD&adminpass-clone=$OWNCLOUD_ADMIN_PASSWORD&directory=$DATA_DIR&dbtype=$DB_TYPE&dbuser=&dbpass=&dbpass-clone=&dbname=&dbhost=localhost"`
 else
-	echo ">> owncloud db already installed"
-#	# update db server
-#	sed -i "s/.*'dbhost' \=>.*/  'dbhost' => '$OWNCLOUD_MYSQL_HOST:$OWNCLOUD_MYSQL_PORT',/g" /var/www/owncloud/config/config.php
-#	# update mail server
-#	sed -i "s/{.*:993/{$OWNCLOUD_IMAP_HOST:993/g" /var/www/owncloud/config/config.php
+  echo ">> using mysql DB"
+	DB_TYPE="mysql"
+	POST=`echo "install=true&adminlogin=$OWNCLOUD_ADMIN&adminpass=$OWNCLOUD_ADMIN_PASSWORD&adminpass-clone=$OWNCLOUD_ADMIN_PASSWORD&directory=$DATA_DIR&dbtype=$DB_TYPE&dbuser=$OWNCLOUD_MYSQL_USER&dbpass=$OWNCLOUD_MYSQL_PASSWORD&dbpass-clone=$OWNCLOUD_MYSQL_PASSWORD&dbname=$OWNCLOUD_MYSQL_DBNAME&dbhost=$OWNCLOUD_MYSQL_HOST:$OWNCLOUD_MYSQL_PORT"`
 fi
+
+echo ">> using wget to post data to owncloud"
+wget -O - --no-check-certificate --no-proxy --post-data "$POST" https://localhost$OWNCLOUD_RELATIVE_URL_ROOT\index.php > /dev/null 2> /dev/null
+
+echo ">> killing nginx - done with configuration"
+sleep 1
+killall nginx
+echo ">> finished initialization"
